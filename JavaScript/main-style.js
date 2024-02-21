6892b30d140d29be89d4fa6c37094bc5548761b1
Daniel Montiel <danielponce1397@gmail.com>
2024-02-20 22:02:37 -0600

    Formulario login-register, html,css y js para animacion,validacion y creacion de usuarios

---
+++ JavaScript/main-style.js
@@ -0,0 +1,136 @@
+//VARIABLES
+
+//SELECTORES
+const contenedorLoginRegister = document.querySelector('.contenedor-login-register');
+
+const formularioInicio = document.querySelector('#formulario-iniciar');
+const formularioRegistro = document.querySelector('#formulario-register');
+
+const containerInicio = document.querySelector('.ingesar-login');
+const containerRegister = document.querySelector('.ingesar-register');
+
+const btnIniciarSesion = document.querySelector('#button-iniciar-sesion');
+const btnRegistrar = document.querySelector('#button-register')
+
+//EVENT LISTENERS
+
+btnRegistrar.addEventListener('click',registrar);
+btnIniciarSesion.addEventListener('click',iniciar);
+
+window.addEventListener('resize',()=>{
+    let width = window.innerWidth;
+    let responsive;
+
+    if(width<800){
+        responsive = true;
+        organizarFormulario(responsive);
+    } else {
+        responsive = false;
+        organizarFormulario(responsive);
+    }
+});
+
+addEventListener('DOMContentLoaded',()=>{
+    let width = window.innerWidth;
+
+    if(width<800){
+        containerRegister.style.display = 'block';
+        containerInicio.style.display = 'none';
+    }
+})
+
+
+//FUNCIONES
+function registrar(){
+    if(window.innerWidth >800){
+        formularioRegistro.style.display = "block";
+        contenedorLoginRegister.style.left = "410px";
+        contenedorLoginRegister.style.top = "-3px";
+        formularioInicio.style.display = 'none';
+        containerRegister.style.opacity = "0";
+        containerInicio.style.opacity ='1';
+    } else{
+        formularioInicio.style.display = 'none';
+        formularioRegistro.style.display = 'block';
+
+        containerRegister.style.display = 'none';
+        containerInicio.style.display = 'block';
+        containerInicio.style.opacity = "1";
+    }
+}
+
+function iniciar(){
+    if(window.innerWidth > 800){
+        formularioRegistro.style.display = "none";
+        contenedorLoginRegister.style.left = "10px";
+        contenedorLoginRegister.style.top = "63px";
+        formularioInicio.style.display = 'block';
+        containerRegister.style.opacity = "1";
+        containerInicio.style.opacity ='0';
+    }  else {
+        formularioInicio.style.display = 'block';
+        formularioRegistro.style.display = 'none';
+
+        containerRegister.style.display = 'block';
+        containerInicio.style.display = 'none';
+        containerRegister.style.opacity = "1";
+    }
+
+}
+
+function organizarFormulario(responsive){
+
+    const formularioInicioDisplay = window.getComputedStyle(formularioInicio).display;
+    const formularioRegistroDisplay = window.getComputedStyle(formularioRegistro).display;
+
+    //Si el tamaño de la ventana es menor a 800px
+    if(responsive === true){
+
+        const botonesEliminar = document.querySelectorAll(".eliminar-usuario");
+        const botonesEditar = document.querySelectorAll(".editar-usuario");
+
+        botonesEditar.forEach(elemento =>{
+            elemento.innerHTML = `<i class="fa-solid fa-pen-to-square"></i>`;
+        })
+
+        botonesEliminar.forEach(elemento => {
+            elemento.innerHTML = `<i class="fa-solid fa-x"></i>`;
+        });
+        
+
+        if(formularioInicioDisplay === 'block'){
+            containerInicio.style.display = 'none';
+        } else if(formularioRegistroDisplay === 'block'){
+            containerRegister.style.display = 'none';
+        }
+
+    }
+
+
+    //Si el tamaño de la ventana es mayor a 800px
+    if(responsive === false){
+        const botonesEliminar = document.querySelectorAll(".eliminar-usuario");
+        const botonesEditar = document.querySelectorAll(".editar-usuario");
+
+        botonesEditar.forEach(elemento =>{
+            elemento.textContent = "Editar";
+        })
+
+        botonesEliminar.forEach(elemento => {
+            elemento.textContent = `Eliminar`;
+        });
+
+        containerInicio.style.display = 'block';
+        containerRegister.style.display = 'block';
+
+        if(formularioInicioDisplay === 'block'){
+            iniciar();
+        } else if(formularioRegistroDisplay === 'block'){
+            registrar();
+        }
+    }
+
+
+}
+
+
---
+++ JavaScript/main.js
@@ -0,0 +1,369 @@
+//VARIABLES
+let usuario;
+let usuarioNuevo;
+let btnEliminar;
+let btnEditar;
+let modoEdicion = false;
+//SELECTORES
+
+//Selectores input "Registro"
+const inputRegisterName = document.querySelector('#name');
+const inputRegisterEmail = document.querySelector('#email-register');
+const inputRegisterPassword = document.querySelector('#password-register');
+const inputRegisterPassword2 = document.querySelector('#password2-register');
+
+const imprimirListaHTML = document.querySelector('#lista-usuarios tbody');
+
+
+
+//Selectores botones
+const btnRegistrarUsuario = document.querySelector('#registrar-usuario');
+
+//OBJETO
+const nuevoUsuario = {
+    nombre: '',
+    email: '',
+    password: ''
+}
+
+//CLASES
+//Clase para almacenar cada usuario que se haya registrado
+class UsuariosRegistrados{
+    constructor(){
+        this.usuarios = [];
+    }
+
+    agregarUsuarioNuevo(usuarioNuevo){
+        this.usuarios = [...this.usuarios, usuarioNuevo];
+        crearListaHTML(this.usuarios);
+    }
+
+    eliminarUsuaroExistente(id){
+        this.usuarios = this.usuarios.filter((usuario) =>usuario.id !==id);
+        crearListaHTML(this.usuarios);
+    }
+
+    editarUsuarioExistente(usuario){
+        this.usuarios = this.usuarios.map(usuarios => usuarios.id === usuario.id ? usuario : usuarios);
+        crearListaHTML(this.usuarios);
+    }
+}
+
+
+//Clase para instanciar los usarios creados
+class UsuarioRegistrado{
+    constructor(nombre,email,password,id){
+        this.nombre = nombre;
+        this.email = email;
+        this.password = password;
+        this.id = id;
+    }
+}
+
+//Clase que se encargará de la interfaz
+class Interface{
+
+    mostrarAlerta(mensaje,tipo,inputParent,input){
+       
+     
+        if(tipo==='edicionCorrecta'){
+            const divMensaje = document.createElement('div');
+            divMensaje.textContent = mensaje;
+            divMensaje.classList.add('div-editando');
+            formularioRegistro.appendChild(divMensaje);
+            setTimeout(()=>{
+                divMensaje.remove();
+            },5000);
+            return;
+        }
+
+        this.limpiarAlerta(inputParent);
+
+
+        const mensajeError = document.createElement('p');
+        mensajeError.textContent = mensaje;
+        
+        if(tipo==='error'){
+            mensajeError.classList.add('campo-obligatorio');
+            input.style.border = '1px solid red';
+        } 
+        
+        if(tipo==='valido'){
+            mensajeError.classList.add('email-valido');
+            input.style.border = '';
+        }
+
+        if(tipo==='correcto'){
+            mensajeError.classList.remove('campo-obligatorio');
+            input.style.border = '';
+            mensajeError.classList.add('email-valido');
+        }
+
+
+
+        inputParent.appendChild(mensajeError);
+    }
+
+    limpiarAlerta(input){
+        if(input.querySelector('p')){
+            input.removeChild(input.lastChild);
+        }
+    }
+
+    agregarListaHTML(usuarios){
+
+        this.limpiarListaHTML();
+
+        //Iteramos sobre cada usuario
+        usuarios.forEach(usuario => {
+            const {nombre,email,id} = usuario;
+
+            //Creamos fila
+            const row = document.createElement('tr');
+            row.dataset.id = id;
+
+            row.innerHTML = `
+            <td>${nombre}</td>
+            <td>${email}</td>
+            `
+
+            //Crear botones para Eliminar y Editar
+            const botones = document.createElement('td');
+            botones.classList.add('botones-editar-eliminar');
+            row.appendChild(botones);
+
+            imprimirListaHTML.appendChild(row);
+
+            btnEliminar = document.createElement('button');
+            btnEliminar.classList.add('eliminar-usuario');
+            btnEliminar.textContent = 'Eliminar';
+
+            botones.appendChild(btnEliminar);
+            btnEliminar.onclick = () =>{
+            eliminarUsuario(id);
+            limpiarObjeto();
+            regresarModoNuevoUsuario();
+            }
+
+            btnEditar = document.createElement('button');
+            btnEditar.classList.add('editar-usuario');
+            btnEditar.textContent = "Editar";
+            botones.appendChild(btnEditar);
+
+
+            btnEditar.onclick = ()=>{
+                editarUsuario(id);
+            }
+        });
+        
+    }
+
+    limpiarListaHTML(){
+        while(imprimirListaHTML.firstChild){
+            imprimirListaHTML.removeChild(imprimirListaHTML.firstChild);
+        }
+    }
+}
+
+//INSTANCIAR
+const interface = new Interface();
+const usuarioNuevoRegistrado = new UsuariosRegistrados();
+
+//EVENT LISTENERS
+
+eventListers();
+function eventListers(){
+    inputRegisterName.addEventListener('input',validarFormulario);
+    inputRegisterEmail.addEventListener('input',validarFormulario);
+    inputRegisterPassword.addEventListener('input',validarFormulario);
+    inputRegisterPassword2.addEventListener('input',validarFormulario);
+
+    btnRegistrarUsuario.addEventListener('click',agregarNuevoUsuario);
+
+}
+
+//FUNCIONES
+
+function regresarModoNuevoUsuario(){
+    btnRegistrarUsuario.textContent = "Registrar";
+    btnRegistrarUsuario.classList.remove('edicion');
+    modoEdicion = false;
+}
+
+function validarFormulario(e){
+
+    let valorInput = e.target.value;
+    const elementoPadre = e.target.parentElement;
+
+    //Validamos si el campo se lleno
+    if(valorInput.trim() === ''){
+        valorInput = '';
+        interface.mostrarAlerta(`El campo ${e.target.name} es obligatorio`,'error',elementoPadre,e.target);
+        nuevoUsuario[e.target.name] = '';
+        //return;
+    }
+
+    //Validamos si el campo email es correcto
+    if(e.target.id==='email-register' && !validarEmail(valorInput)){
+        interface.mostrarAlerta('El email no es valido','error',elementoPadre,e.target);
+        nuevoUsuario[e.target.name] = '';
+        return;
+    } 
+
+    //Si el email es llenado correctamente mandamos el mensaje de valido
+    if(e.target.id==='email-register' && validarEmail(valorInput)){
+        interface.mostrarAlerta('Email válido','valido',elementoPadre,e.target);
+        nuevoUsuario[e.target.name] = valorInput;
+        
+    }
+
+
+    // VALIDAR CONTRASEÑAS
+    //Revisamos si las contraseñas coinciden
+    if(e.target.id === 'password-register' && valorInput.trim() != ''){
+        //Quitamos la alerta si tiene algo el input
+        interface.mostrarAlerta('','correcto',elementoPadre,e.target);
+
+        //Si las contraseñas no coinciden mandamos error en password2
+        if(valorInput != inputRegisterPassword2.value){
+            interface.mostrarAlerta('Las contraseñas no coinciden','error',inputRegisterPassword2.parentElement,inputRegisterPassword2);
+            nuevoUsuario[e.target.name] = '';
+            return;
+        } else{
+            interface.mostrarAlerta('Las contraseñas coinciden','correcto',inputRegisterPassword2.parentElement,inputRegisterPassword2);
+            nuevoUsuario[e.target.name] = valorInput;
+            
+        }
+    }
+
+    //Al salir del input password2 revisamos si las contraseñas son correctas.
+    if(e.target.id==='password2-register' && valorInput.trim()){
+        if(inputRegisterPassword.value != inputRegisterPassword2.value){
+            interface.mostrarAlerta('La contraseña no coincide','error',elementoPadre,e.target);
+            nuevoUsuario[e.target.name] = '';
+            return;
+        } else{
+            interface.mostrarAlerta('La contraseña coincide','correcto',elementoPadre,e.target);
+            nuevoUsuario[e.target.name] =valorInput;
+            
+        }
+    }
+
+
+    //Si un campo se lleno mandamos el mensaje de llenado correctamente
+
+    if(valorInput.trim() && e.target.id === 'name'){
+        interface.mostrarAlerta(`Campo ${e.target.name} llenado correctamente`,'correcto',elementoPadre,e.target);   
+        nuevoUsuario[e.target.name] = valorInput;
+         
+    }
+
+    //Cada que un campo sea correcto revisamos si ya se llenaron todos los campos correctamente
+    comprobarRegistro();
+}
+
+//Validar si email tiene formato de email
+ function validarEmail(email){
+    const regex =  /^\w+([.-_+]?\w+)*@\w+([.-]?\w+)*(\.\w{2,10})+$/ 
+        const resultado = regex.test(email);
+        return resultado;
+ }
+
+ //Comprobamos si todos los datos ya fueron agregados correctamente
+function comprobarRegistro(){
+    if(Object.values(nuevoUsuario).includes('')){
+        btnRegistrarUsuario.disabled = true;
+        btnRegistrarUsuario.opacity = 0.5;
+    } else{
+        btnRegistrarUsuario.disabled = false;
+        btnRegistrarUsuario.opacity = 0;
+    }
+}
+
+
+ function agregarNuevoUsuario(e){
+    e.preventDefault();
+    
+
+    if(modoEdicion === false){
+        
+        nuevoUsuario['id'] = Date.now();
+        const {nombre,email,password,id} = nuevoUsuario;
+    
+        const usuario = new UsuarioRegistrado(nombre,email,password,id);
+    
+        usuarioNuevoRegistrado.agregarUsuarioNuevo(usuario);
+
+    } else{
+        interface.mostrarAlerta('Editado Correctamente',"edicionCorrecta",'','');
+
+        const {nombre,email,password,id} = nuevoUsuario;
+
+        const usuario = new UsuarioRegistrado(nombre,email,password,id);
+
+        usuarioNuevoRegistrado.editarUsuarioExistente({...usuario});
+
+        regresarModoNuevoUsuario();
+        organizarFormulario(true);
+        
+    }
+    
+
+    limpiarObjeto();
+    comprobarRegistro();
+    limpiarAlertas();
+
+ };
+
+function limpiarObjeto(){
+        //Limpiar objeto
+        formularioRegistro.reset();
+        nuevoUsuario.nombre = '';
+        nuevoUsuario.email = '';
+        nuevoUsuario.password = '';
+        delete nuevoUsuario.id;
+} 
+
+function limpiarAlertas(){
+    interface.limpiarAlerta(inputRegisterName.parentElement);
+    interface.limpiarAlerta(inputRegisterEmail.parentElement);
+    interface.limpiarAlerta(inputRegisterPassword2.parentElement);
+}
+
+function crearListaHTML(usuarios){
+     interface.agregarListaHTML(usuarios);
+    }
+
+function eliminarUsuario(id){
+    usuarioNuevoRegistrado.eliminarUsuaroExistente(id);
+}
+
+function editarUsuario(id){
+    const usuarioEditar = usuarioNuevoRegistrado.usuarios.find((usuario)=>usuario.id === id);
+
+    const {nombre,email,password} = usuarioEditar;
+
+    //Agregamos el objeto a editar al formulario
+    formularioRegistro.reset();
+    registrar();
+    limpiarAlertas();
+
+    inputRegisterName.value = nombre;
+    inputRegisterEmail.value = email;
+    inputRegisterPassword.value = password;
+    inputRegisterPassword2.value = password;
+
+    //Llenamos el objeto
+    nuevoUsuario.nombre = nombre;
+    nuevoUsuario.email = email;
+    nuevoUsuario.password = password;
+    nuevoUsuario.id = id;
+    comprobarRegistro();
+
+    btnRegistrarUsuario.classList.add('edicion');
+    btnRegistrarUsuario.textContent = "Guardar Cambios";
+
+    modoEdicion = true;
+
+    //usuarioNuevoRegistrado.editarUsuarioExistente(id);
+}
